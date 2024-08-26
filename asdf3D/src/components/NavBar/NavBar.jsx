bfced65e83588c3384ff4c280dc45202321c55a3
Pablo Estrada <pablo.estrada.89@gmail.com>
2024-08-26 06:13:09 -0300

    CreaTuLanding1+Estrada

---
+++ asdf3D/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <div>
+        <nav>
+            <il>
+                <ul>1</ul>
+                <ul>1</ul>
+                <ul>1</ul>
+            </il>
+        </nav>
+    </div>
+  )
+}
